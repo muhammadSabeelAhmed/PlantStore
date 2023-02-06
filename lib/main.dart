552756b7b00f2cb04ee58b98ae261f671eee2700
sabeel804640 <msa_malik@rocketmail.com>
2023-02-06 17:39:41 +0500

    splash added

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(home: SplashScreen());
   }
 }
